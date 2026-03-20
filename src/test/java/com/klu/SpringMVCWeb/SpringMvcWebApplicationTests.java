ef41d3901523fa82d22aa7b9af01475380f34d1f
Your Name <your@email.com>
2026-03-20 16:08:32 +0530

    Initial commit

---
+++ src/test/java/com/klu/SpringMVCWeb/SpringMvcWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klu.SpringMVCWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
